65e2be21ddaa22b10a4ba19371e99d998754d99e
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-07-18 14:11:22 -0400

    adjusted the .md language and added licensing info

--- index.js
+++ index.js
@@ -4,78 +4,148 @@ const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const questions = [];
-// function call to initialize program
-//init();
+//function call to initialize program
+init();
 
-// function to initialize program
-// async function init() {
-//   try {
-//     const answers = await promptUser();
-//     const html = generateREADME(generateREADME);
+//function to initialize program
+async function init() {
+  try {
+    const answers = await promptUser();
+    //  const text = generateREADME(answers);
 
-//     await fs.writeFileSync("README.md", answers);
-//   } catch (err) {
-//     console.log("ERR", err);
-//   }
-// }
-promptUser();
+    await writeFileAsync("README.md", generateREADME(answers));
+  } catch (err) {
+    console.log("ERR", err);
+  }
+}
+
 // array of questions for user
 function promptUser() {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "How would you describe your project?",
-      },
-      {
-        type: "input",
-        name: "install",
-        message: "Are there any installation instructions for this project?",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "Any additional usage information?",
-      },
-      {
-        type: "input",
-        name: "contribute",
-        message: "What are the contribution guidelines?",
-      },
-      {
-        type: "input",
-        name: "tests",
-        message: "What tests have been run?",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What is the github repository link?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
-      },
-    ])
-    .then(function (answers) {
-      fs.writeFile("README.md", JSON.stringify(answers, null, "\t"), function (
-        err
-      ) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("success");
-      });
-    });
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "rawlist",
+      name: "license",
+      message: "Select your license:",
+      choices: ["MIT", "IBM", "Mozilla", "None"],
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "How would you describe your project?",
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "Are there any installation instructions for this project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Any additional usage information?",
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message: "What are the contribution guidelines?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What tests have been run?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the github repository link?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+  ]);
 }
 
 // function to write README file
-function writeToFile(fileName, data) {}
+
+function generateREADME(answers) {
+  return `
+${
+  answers.license === "MIT"
+    ? "![](https://img.shields.io/badge/license-MIT-brightgreen)"
+    : ""
+}
+${
+  answers.license === "IBM"
+    ? "![](https://img.shields.io/badge/License-IPL%201.0-blue.svg)"
+    : ""
+}
+${
+  answers.license === "Mozilla"
+    ? "![](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)"
+    : ""
+}
+
+# ${answers.title} 
+      
+## Table of Contents:
+      
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions?](#created-by)
+      
+  
+## Description: 
+${answers.description}
+
+      
+## Installation: 
+${answers.install}
+      
+
+      
+## Usage: 
+${answers.usage}
+
+
+## License:
+${
+  answers.license === "MIT"
+    ? "MIT: A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
+    : ""
+}
+      
+
+     
+## Contributing:
+${answers.contribute}
+      
+
+    
+## Tests: 
+${answers.tests}
+      
+
+## Created by: 
+${answers.name}
+
+${answers.github}
+
+${answers.email}
+      
+`;
+}
